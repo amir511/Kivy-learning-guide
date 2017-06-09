231756aa760a0a91fe3b6747601c9a9555bef575
Amir <amir.anwar.said@gmail.com>
2017-06-09 02:05:49 +0200

    some testing and learning

--- testing.py
+++ testing.py
@@ -1,28 +1,20 @@
-import os
-os.environ["KIVY_NO_CONSOLELOG"]= "1"
-import kivy
-kivy.require('1.10.0')
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout  # The Grid Layout will be the container of the prepared widget
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput    # This is a user input widget
+from kivy.uix.gridlayout import GridLayout
 
-class Login_screen(GridLayout):     # Now defining the prepared widget
+class Screen(GridLayout):
     def __init__(self,**kwargs):
-        super(Login_screen,self).__init__(**kwargs)     # this line is mandatory or the app won't work, this is related to the base class
-        self.cols = 2   # this setups the GridLayout to have two columns, the other widgets will be inserted in those columns
-        self.add_widget(Label(text = 'User Name'))      # This method inserts a Label widget in the first row of the first column
-        self.username = TextInput(multiline=False)      # This will give our Login_screen class an attribute called username
-        self.add_widget(self.username)  # with the previous line, this can be done in only one line: 
-        # self.add_widget(TextInput(multiline=False))
-        self.add_widget(Label(text='Password')) 
-        self.password = TextInput(password=True,multiline=False)
-        self.add_widget(self.password)  # inserting last widget in the second row in the second column
+        return super(Screen,self).__init__(**kwargs)
+        output = StringProperty(Label)
+        inputt = StringProperty(TextInput)
+    # def get_text(self,text):
+    #     output.text = inputt.text
 
-class Amir_App(App):
+
+class TestApp(App):
     def build(self):
-        self.title = 'My First Kivy Application'    # adding a title to the window
-        return Login_screen()   # returning a widget (our prepared Login_screen widget)
+        return Screen()
 
-mero = Amir_App()
-mero.run()  # Can be done like this: Amir_App().run(): instantiation on the fly and calling run method immediately
+TestApp().run()
