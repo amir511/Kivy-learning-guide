dc8d774dfee90620d77a2bebc945b9fed762ecdf
Amir <amir.anwar.said@gmail.com>
2017-06-20 23:34:29 +0200

    Added explanation for scheduling in the kivy summary

--- Kivy_Summary.md
+++ Kivy_Summary.md
@@ -264,7 +264,40 @@ If you request a web page to be loaded or downloaded while you are in the *Main 
 
 Scheduling is crucial to avoid UI hanging or bad user experience
 
-<!--Start Adding how to make a scheule based on the testing.py file-->
+**How Scheduling works?**
+
+There are many ways to implement scheduling in kivy, but the common ways are:
+
+```python
+#....
+from kivy.clock import Clock
+# Method 1:
+event = Clock.schedule_interval(function, interval)   # This method will trigger the function every some interval
+
+# But the function passed here must have a patameter of delta time as an argument in its signature
+
+def function(dt):
+    # some code
+# the dt parameter is not needed to be used in the function
+
+# Method 2:
+event = Clock.schedule_once(function, delay)    # This will trigger function once only after delay seconds
+
+# Again this function should have 'dt' as an argument
+# Off course this function is part of a class, it should look like this:
+
+def function(self,dt):
+    # some code
+
+# If at some condition or at some point, you need to disable the scheduled event, i.e. unschedule it, use:
+event.cancel()
+
+# Or:
+Clock.unschedule(event)
+
+# Or in the signature of the function itself, return False at the desired cancelation condition
+
+```
 
 ## Controling the environment
 ```python
--- testing.py
+++ testing.py
@@ -14,10 +14,15 @@ Config.set('graphics','height',200)
 
 class MyScreen(GridLayout):
     now = str(time.localtime()[3])+':'+str(time.localtime()[4])+':'+str(time.localtime()[5])
+    trigger = None
     def __init__(self,**kwargs):
         super(MyScreen, self).__init__(**kwargs)
         self.repeat_this(1)
-    
+        self.create_trigger()
+        self.trigger()
+        Clock.schedule_once(self.fill_label,12)
+        Clock.schedule_once(self.clear_label,14)
+                        
     def repeat_this(self,interval):
         Clock.schedule_interval(self.print_now,interval)
     
@@ -28,15 +33,26 @@ class MyScreen(GridLayout):
     def update_now(self):   
         now = str(time.localtime()[3])+':'+str(time.localtime()[4])+':'+str(time.localtime()[5])
         self.now = now
+    
+    
+    def create_trigger(self):
+        global trigger
+        trigger = Clock.create_trigger(self.clear_label, 10)
+    
+    
+    def clear_label(self,dt):
+        self.ids.label.text = ""
+    
+    def trigger(self):
+        global trigger
+        trigger()
+    
+    def fill_label(self,dt):
+        self.ids.label.text = "New text added"
 
 class MyApp(App):   # this class name should always be like this: SomenameApp
     def build(self):
         return MyScreen()
 
-# class callback(CyClockBase):
-    
-#     def __init__(self):
-#         now = str(time.localtime()[3])+':'+str(time.localtime()[4])+':'+str(time.localtime()[5])
-#         print(now)
 if __name__ == '__main__':
     MyApp().run() #Instantiating the app on the fly and calling its run method
